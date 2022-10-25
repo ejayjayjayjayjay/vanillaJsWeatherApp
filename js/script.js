81aa4b84aa49444f7ee260388dbeaaf7ba4cc461
Josell Vibar <vibarjosell@gmail.com>
2022-10-25 21:40:58 +0800

    WIP: display weather property

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 //https://api.openweathermap.org/data/2.5/weather?q=manila&units=metric&appid=9f7e9e0b314b24c7c6eaf82ebc88862a
 
-let weather = {
+/* let weather = {
     apiKey: '9f7e9e0b314b24c7c6eaf82ebc88862a',
     fetchWeather: (() => {
         fetch('https://api.openweathermap.org/data/2.5/weather?q=manila&units=metric&appid=9f7e9e0b314b24c7c6eaf82ebc88862a').then
@@ -10,4 +10,27 @@ let weather = {
                 })
             })
     })
-}
+} */
+
+let weather = {
+    apiKey: "9f7e9e0b314b24c7c6eaf82ebc88862a",
+    fetchWeather: (city) => {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" +
+            city +
+            "&units=metric&appid=" +
+            this.apiKey
+        ).then((Response) => {
+            Response.json().then((data) => {
+                this.displayWeather();
+            });
+        });
+    },
+    displayWeather: () => {
+        const { name } = data;
+        const {icon, description} = data.weather[0];
+        const {temp, humidity} = data.main;
+        const {speed} = data.wind;
+        console.log(name, icon, description , temp, humidity , speed);
+    },
+};
