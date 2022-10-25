b1b22da8f1942b299482ab37a73ce2b93787b2c8
Josell Vibar <vibarjosell@gmail.com>
2022-10-25 20:56:33 +0800

    test api to print data on console just for testing

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
+//https://api.openweathermap.org/data/2.5/weather?q=manila&units=metric&appid=9f7e9e0b314b24c7c6eaf82ebc88862a
+
 let weather = {
     apiKey: '9f7e9e0b314b24c7c6eaf82ebc88862a',
-    
+    fetchWeather: (() => {
+        fetch('https://api.openweathermap.org/data/2.5/weather?q=manila&units=metric&appid=9f7e9e0b314b24c7c6eaf82ebc88862a').then
+            ((Response) => {
+                Response.json().then((data) => {
+                    console.log(data);
+                })
+            })
+    })
 }
