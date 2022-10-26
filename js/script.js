1ea4450a155c83a62d0a04ba9bffe94b46fa6cda
Josell Vibar <vibarjosell@gmail.com>
2022-10-26 19:43:49 +0800

    update js

--- js/script.js
+++ js/script.js
@@ -12,41 +12,17 @@
     })
 } */
 
-/* let weather = {
-    apiKey: "9f7e9e0b314b24c7c6eaf82ebc88862a",
-    fetchWeather: (city) => {
-        fetch(
-            "https://api.openweathermap.org/data/2.5/weather?q=" +
-            city +
-            "&units=metric&appid=" +
-            this.apiKey
-        ).then((Response) => {
-            Response.json().then((data) => {
-                this.displayWeather(data);
-            });
-        });
-    },
-    displayWeather: () => {
-        const { name } = data;
-        const {icon, description} = data.weather[0];
-        const {temp, humidity} = data.main;
-        const {speed} = data.wind;
-        console.log(name, icon, description , temp, humidity , speed);
-    },
-}; */
-
-
 let weather = {
     apiKey: "9f7e9e0b314b24c7c6eaf82ebc88862a",
     fetchWeather: function (city) {
-      fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city +"&units=metric&appid=" + this.apiKey)
-      .then((response) => response.json()).then((data) => this.displayWeather(data));
+        fetch("https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=" + this.apiKey)
+            .then((response) => response.json()).then((data) => this.displayWeather(data));
     },
-    displayWeather: function () {
-      const {name} = data;
-      const {icon,description} = data.weather[0];
-      const {temp, humidity} = data.main;
-      const {speed} = data.wind;
-      console.log(name, icon, description, temp, humidity, speed);
+    displayWeather: function (data) {
+        const { name } = data;
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name, icon, description, temp, humidity, speed);
     }
-  };
+};
